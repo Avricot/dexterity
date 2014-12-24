bf04babdaa2825746f319869e2df6375f4519ed4
Quentin Ambard <quentin.ambard@gmail.com>
2014-12-24 19:46:29 +0100

    add remote

--- game-smart-remot.js
+++ game-smart-remot.js
@@ -1,40 +1,14 @@
-X=0;
-//BALL SUPPLEMENTAIRES, copie ajoutées
-var ball1=true;
-var ball2=false;
-var ball3=false;
-var ball4=true;
-var ball5=true;
-
 var t = Date.now();
 var r = 0;
 var modulo = 1;
 var trace = [];
-var depth = 80;
-serverPlayer2PosX=0.5;
+var depth = 70;
 var requestAnimationFrameFrequency = 1e3 / 60 //40; //Executé tous les 16 ms
 done = false;
 var resolution=100;
 var lastX=0;
 
 function ballUpdate(){
-    //ICI dans balls tu as un tableau avec toutes les balles disponibles.
-    //Le but du jeux : faire bouger X (il peut aller de 0 à WIDTH=400) pour qu'il évite d'être en collision avec une ball.
-    //Zone de collision: rectangle de hauteur 55 et largeur 30. Position sur le canvas : ctx.rect(0+X,265,30,55);
-    //Le haut des boules est a 40
-    //Le plus bas des boules est à 278
-    //temps depuis dernier
-    //Acceleration moyenne (avec y1 variant de 0->1) : -0.000018253172955523
-//    var acceleration = -0.000018253172955523;
-//    var dt = Date.now()-t;
-//    t = Date.now();
-//    dy = balls[0].y1-lastY;
-//    var v=dy/dt;
-//    var dv = v-lastV;
-//    var a = dv/dt
-    //Log les resultats pour afficher sous excel
-    //console.log(dt+","+t+","+balls[0].x1+","+balls[0].y1);
-    //Cacule les positions futures. On prend les position des 10 dt suivantes
     var dt = (Date.now()-t)/1000;
     t= Date.now();
     if(mBalls.length>0 && dt>0){
@@ -62,15 +36,8 @@ function ballUpdate(){
                 if(!p[i]){
                     p[i] = [];
                 }
-                // p[i][j] = true;
             } else {
-                //TODO revoir le minimum ici
-//                console.log("p["+i+"]["+j+"]:")
-//                console.log(p[i][j])
                 var copy = p[i][j].slice();
-//                if(minDistance < copy[copy.length-1]){
-//                    copy[copy.length-1] = minDistance;
-//                }
                 copy[0] += minDistance;
                 return copy;
             }
@@ -79,7 +46,6 @@ function ballUpdate(){
             for (var t = 0; t < cachePosition[j].length; t++){
                 var ball = cachePosition[j][t];
                 //distance uniquement si la balle descend ?
-//            if(ball[2]<0){
                 var distance = (ball[0]-position)*(ball[0]-position) + (ball[1]-PLAYER_SIZE)*(ball[1]-PLAYER_SIZE);
                 if(j>10){
                     distance = distance/3;
@@ -96,33 +62,18 @@ function ballUpdate(){
                 if(distance<0){
                     distance = distance*-1;
                 }
-            //}
-//                    if(distance < minDistance){
-//                        minDistance = distance ;
-//                    }
                 distanceM += distance;
 
                 if(ball[0] < position + 1.5 * (BALL_SIZE / 2 + PLAYER_SIZE / 2)
                     && ball[0] > position - 1.5 * (BALL_SIZE / 2 + PLAYER_SIZE / 2)
                     && ball[1] < 2.3 * PLAYER_SIZE){
                     collision = true ;
-                    //console.log("coooolision-------------------------------------------------------------------------")
                 }
             }
             if(!collision){
                 var pathLeft = calcPath(position-dx, i-1, j+1);
                 var pathRight = calcPath(position+dx, i+1, j+1);
                 var pathDown = calcPath(position, i, j+1);
-//                if(pathDown.length >= depth){
-//                    return pathDown;
-//                }
-                if(j<1){
-//                    console.log(pathDown)
-//                    console.log(pathLeft)
-//                    console.log(pathRight)
-//                    console.log(j+"----------");
-                }
-
                 var returnedPath = null;
 
                 if(pathDown.length > pathLeft.length && pathDown.length > pathRight.length){
@@ -133,11 +84,6 @@ function ballUpdate(){
                 }
                 if(pathRight.length > pathDown.length && pathRight.length > pathLeft.length){
                     returnedPath = pathRight;
-                }
-                if(j == 0){
-//                    console.log(minDown);
-//                    console.log(minLeft);
-//                    console.log(minRight);
                 }
                 var minDown = pathDown[0];
                 var minLeft = pathLeft[0];
@@ -156,15 +102,7 @@ function ballUpdate(){
                         returnedPath = pathDown;
                     }
                 }
-//
-//                if(serverPlayer2PosX<0.5){
-//                    return pathRight;
-//                } else if(serverPlayer2PosX>0.5){
-//                    return pathLeft;
-//                }
                 p[i][j] = returnedPath;
-//                console.log("returnedPath:p["+i+"]["+j+"]:")
-//                console.log(returnedPath)
                 returnedPath = returnedPath.slice();
                 returnedPath.push(i);
                 returnedPath[0] = returnedPath[0]+distance;
@@ -173,66 +111,81 @@ function ballUpdate(){
             return [0];
         }
         //Position de 1 à 100.
-        var index = Math.round(serverPlayer2PosX*resolution);
-//        if(!done){
-            var bestPath = calcPath(serverPlayer2PosX, index, 0);
-//            done = true;
-//        }
-//        console.log(bestPath)
+        var index = Math.round(serverPlayerPosX*resolution);
+        var bestPath = calcPath(serverPlayerPosX, index, 0);
         if(bestPath.length<3){
+            if(lastX != 0){
+                socket.emit("commande", 0);
+                lastX = 0;
+            }
             console.log("OK ca déconne");
-            console.log(trace)
         } else {
-//            if(bestPath[bestPath.length-2] != 50)
-//            trace.push(bestPath)
-            var positionNumber = dx*100;
-//            var direction = bestPath[bestPath.length-2]-index;
-//            for(var i=0;i<positionNumber;i++){
-//                if(bestPath.length-2-i>0){
-//                    direction+=bestPath[bestPath.length-2-i]-bestPath[bestPath.length-2-i+1];
-//                }
-//            }
-//            var dir = 0;
-//            if(direction>0){
-//                dir = 1;
-//            } else if (direction<0){
-//                dir = -1;
-//            }
-            //move(dir, positionNumber*10/1000);
-            move(bestPath[bestPath.length-2]-index, positionNumber/100);
+            move(bestPath[bestPath.length-2]-index);
         }
-        function move(x, dt){
-            if(x!=0){
-//                console.log(x);
-//                console.log(dt);
-//                console.log("------------");
-            }
-            serverPlayer2PosX += dt*x;
-            //On ne peut pas dépasser
-//            console.log(dx*x);
-//            console.log(x);
-//            console.log(serverPlayer2PosX);
-            serverPlayer2PosX = Math.max(PLAYER_SIZE/2,Math.min(serverPlayer2PosX, 1-PLAYER_SIZE));
-//            console.log(serverPlayer2PosX);
-//            console.log("----------------------------------");
-            var n = WIDTH * PLAYER_SIZE;
-
-            for (var t = 0; t < mBalls.length; t++){
-                var ball = $.extend(true, {}, mBalls[t]);
-                if(ball[0] < serverPlayer2PosX + .7 * (BALL_SIZE / 2 + PLAYER_SIZE / 2)
-                    && ball[0] > serverPlayer2PosX - .7 * (BALL_SIZE / 2 + PLAYER_SIZE / 2)
-                    && ball[1] < 1.7 * PLAYER_SIZE){
-                    console.log("coooolision-------------------------------------------------------------------------")
+        function move(x){
+            if(lastX != x){
+                if(x!=0){
+                    socket.emit("commande", 0);
                 }
+                socket.emit("commande", x);
             }
-//            socket.emit("commande", x)
-//            setTimeout(function () {
-//                socket.emit("commande", 0)
-//            }, dx-2);
+            lastX = x;
         }
     }
-    r = ++r;
 }
+
+function insertMBalls(e, n) {
+    ballUpdate();
+    if (!n) {
+        for (var t = 0; t < mBalls.length; t++){
+            mBalls[t][0] = e[t][0];
+            mBalls[t][1] = e[t][1];
+            mBalls[t][2] = e[t][2];
+            mBalls[t][3] = e[t][3];
+            mBalls[t][4] = e[t][4];
+        }
+    }
+    for (var a = mBalls.length; mBalls.length < e.length;) {
+        var s = [];
+        s[0] = e[a][0];
+        s[1] = e[a][1];
+        s[2] = e[a][2];
+        s[3] = e[a][3];
+        s[4] = e[a][4];
+        mBalls.push(s), a++
+    }
+    for (; mBallsRotation.length < mBalls.length;){
+        mBallsRotation.push(0)
+    }
+}
+
+function updateBallPosition(dt, ball) {
+    ball[0] += ball[2] * dt;
+    ball[1] += -1.1 * dt * dt / 2 + ball[3] * dt;
+    ball[3] += -1.1 * dt; //vitesse
+    ball[0] < BALL_SIZE / 2 && ball[2] < 0 && (ball[2] = -ball[2]);
+    ball[0] > 1 - BALL_SIZE / 2 && ball[2] > 0 && (ball[2] = -ball[2]);
+    ball[1] < BALL_SIZE / 2 && (ball[3] = ball[4], ball[1] = BALL_SIZE / 2);
+}
+
+
+
+
+
+
+/* --------------------------- */
+
+
+
+
+
+
+
+
+
+
+
+
 function playParty(orders) {
     var i =0;
     play();
@@ -358,6 +311,8 @@ function updateBallPosition(dt, ball) {
     ball[0] < BALL_SIZE / 2 && ball[2] < 0 && (ball[2] = -ball[2]);
     ball[0] > 1 - BALL_SIZE / 2 && ball[2] > 0 && (ball[2] = -ball[2]);
     ball[1] < BALL_SIZE / 2 && (ball[3] = ball[4], ball[1] = BALL_SIZE / 2);
+//            ball[2] > 0 && (mBallsRotation[t] += 100 * dt, mBallsRotation[t] > 360 && (mBallsRotation[t] -= 360));
+//        ball[2] < 0 && (mBallsRotation[t] -= 100 * dt, mBallsRotation[t] < 0 && (mBallsRotation[t] += 360))
 }
 
 
@@ -412,9 +367,7 @@ function render() {
 }
 
 function renderBackground() {
-    if(r % modulo == 0){
-        ctx.drawImage(spriteBackground, 0, 0, 400, 400)
-    }
+    ctx.drawImage(spriteBackground, 0, 0, 400, 400)
 }
 
 function renderBlackFinish(e) {
@@ -422,18 +375,16 @@ function renderBlackFinish(e) {
 }
 
 function renderBalls(e) {
-    if(r % modulo == 0){
-        var n = WIDTH * BALL_SIZE;
-        for (var t = 0; t < e.length; t++){
-            ctx.save();
-            ctx.translate(e[t][0] * WIDTH, .8 * HEIGHT - e[t][1] * WIDTH);
-            ctx.rotate(mBallsRotation[t] * Math.PI / 180);
-            ctx.translate(-(e[t][0] * WIDTH), -(.8 * HEIGHT - e[t][1] * WIDTH));
-            var ballX = e[t][0] * WIDTH - n / 2;
-            var ballY = .8 * HEIGHT - e[t][1] * WIDTH - n / 2;
-            ctx.drawImage(spriteRock, ballX, ballY, n, n);
-            ctx.restore()
-        }
+    var n = WIDTH * BALL_SIZE;
+    for (var t = 0; t < e.length; t++){
+        ctx.save();
+        ctx.translate(e[t][0] * WIDTH, .8 * HEIGHT - e[t][1] * WIDTH);
+        ctx.rotate(mBallsRotation[t] * Math.PI / 180);
+        ctx.translate(-(e[t][0] * WIDTH), -(.8 * HEIGHT - e[t][1] * WIDTH));
+        var ballX = e[t][0] * WIDTH - n / 2;
+        var ballY = .8 * HEIGHT - e[t][1] * WIDTH - n / 2;
+        ctx.drawImage(spriteRock, ballX, ballY, n, n);
+        ctx.restore()
     }
     //ballUpdate();
 }
@@ -441,9 +392,7 @@ function renderBalls(e) {
 function renderPlayer(e) {
     var n = WIDTH * PLAYER_SIZE;
     //ctx.drawImage(spritePlayer, e * WIDTH - n / 2, .8 * HEIGHT - 2 * n, n, 2 * n)
-    if(r % modulo == 0){
-        ctx.drawImage(spritePlayer2, serverPlayer2PosX * WIDTH - n / 2, .8 * HEIGHT - 2 * n, n, 2 * n);
-    }
+    ctx.drawImage(spritePlayer2, serverPlayer2PosX * WIDTH - n / 2, .8 * HEIGHT - 2 * n, n, 2 * n);
 }
 
 function renderGameInfo(e, n, t) {
